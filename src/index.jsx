63784b4c437c08445bfb59296ec7d151ac4f9263
ridermansb <ridermansb@gmail.com>
2020-08-24 10:14:55 -0300

    Include version and developer on console.log fix #20

--- src/index.jsx
+++ src/index.jsx
@@ -8,4 +8,15 @@ import App from './App';
 
 const root = document.querySelector('#root');
 
+console.log(
+    `%cEvalBox%cversion %c${__VERSION__}`,
+    'color: #fff; background-color: #3864af; padding: 2px 4px; border-radius: 2px',
+    'margin-left: 2px; color: #000000f2',
+    'font-weight: bold; color: #000000f2'
+);
+console.log(
+    `%cby @ridermansb (ridermansb@gmail.com)`,
+    'color: #3864af; font-style: italic; font-size: 10px;'
+);
+
 render(<App />, root);
